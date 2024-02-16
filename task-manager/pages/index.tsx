5fc1d5b1a4112641b5af960f3142e5f01a593a89
Lance H <lsaucyfx@gmail.com>
2024-02-15 20:21:11 -0500

    added css to index.tsx

--- task-manager/pages/index.tsx
+++ task-manager/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from '../styles/styles.module.css'
 
 const HomePage: React.FC = () => {
     return (
 
-        <div>
-          <h1>
+        <div className={styles.container}>
+          <h1 className={styles.heading}>
             Welcome to Web Task Manager
           </h1>
         </div>
