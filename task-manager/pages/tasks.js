2820afca9947a74918e19c29dbf4b083ec59c098
Lance H <lsaucyfx@gmail.com>
2024-02-15 19:58:41 -0500

    added tasks page

---
+++ task-manager/pages/tasks.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TaskList from "../app/components/TaskList";
+
+const TasksPage = () => {
+        
+    return (
+        <div>
+          <h1>Task Manager</h1>
+          <TaskList />
+        </div>
+    );
+};
+
+export default TasksPage;
