88f0eccbf6928c5c5a52e3fe77c6e54693ee31fe
Lance H <lsaucyfx@gmail.com>
2024-02-15 19:55:47 -0500

    added TaskForm component

--- task-manager/app/components/TaskForm.js
+++ task-manager/app/components/TaskForm.js
@@ -1,1 +1,29 @@
 import React, { useState } from "react";
+
+const TaskForm = ({ addTask }) => {
+    const [newTask, setNewTask] = useState('');
+
+    const handeSubmit = (e) => {
+        e.preventDefault();
+        if (newTask.trim() !== '') {
+            addTask({id: Date.now(), title: newTask});
+            setNewTask('');
+        }
+    };
+
+    return (
+        <form onSubmit={handeSubmit}> 
+            <label>
+                New Task:
+                <input
+                type="text"
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+                />
+            </label>
+            <button type="submit">Add Task</button>
+        </form>
+    );
+};
+
+export default TaskForm;
